7662f96b3e6e7fe4d2d21626abf5e6b27deb6f43
bill <jy@ozzis.us>
2017-01-30 16:14:50 -0500

    s2-l25

---
+++ first-app/src/app/databinding/event-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'fa-event-binding',
+  template: `
+    <button (click)="onClicked()">click me</button>
+  `,
+  styles: []
+})
+export class EventBindingComponent  {
+  onClicked(){
+    alert('hi');
+  }
+
+}
