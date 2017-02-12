8671ff95152080d82bfcd7635f30f4f04723addf
bill r <jy@ozzis.us>
2017-02-12 13:07:27 -0500

    s4-l45

--- 02-directives/src/app/highlight.directive.ts
+++ 02-directives/src/app/highlight.directive.ts
@@ -1,14 +1,26 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
+  @HostListener('mouseenter') mousein(){
+    this.bgcolor = 'green';
+  }
 
+  @HostListener('mouseleave') mouseout(){
+    this.bgcolor = 'white';
+  }
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer) {
+  @HostBinding('style.backgroundColor') get setColor(){
+    return this.bgcolor;
+  }
+
+  private bgcolor = 'white';
+
+  constructor() {
     // this.elementRef.nativeElement.style.backgroundColor = '#fdd';
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'backgroundColor', 'pink');
+    // this.renderer.setElementStyle(this.elementRef.nativeElement, 'backgroundColor', 'pink');
   }
 
 }
