a5f9fe8688eb92af659763a7deaa1c0807ff90ac
bill r <jy@ozzis.us>
2017-02-12 13:46:53 -0500

    s4-l48

--- 02-directives/src/app/app.component.ts
+++ 02-directives/src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private swatch = true;
+
+  onSwitch(){
+   this.swatch = !this.swatch;
+  }
 }
