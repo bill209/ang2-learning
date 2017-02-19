85c3f09019058db5caed17ab77eb0a0c15de4a9b
bill r <jy@ozzis.us>
2017-02-19 13:06:52 -0500

    s9-l77

--- RoutingStart/src/app/user/user.component.ts
+++ RoutingStart/src/app/user/user.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnDestroy } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
+      <button (click)="goHome()">go home</button>
     `
 })
 export class UserComponent {
+
+  constructor(private router: Router){}
+  goHome(){
+		this.router.navigate(['']);
+  }
 }
