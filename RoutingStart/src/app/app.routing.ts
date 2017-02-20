4405e7449e6ca5c3abfe3a054d92b28ab08839e4
bill r <jy@ozzis.us>
2017-02-20 12:13:17 -0500

    s9-l87

--- RoutingStart/src/app/app.routing.ts
+++ RoutingStart/src/app/app.routing.ts
@@ -5,9 +5,13 @@ import {ModuleWithProviders} from "@angular/core";
 import {USER_ROUTES} from "./user/user.routes";
 
 const APP_ROUTES: Routes = [
+	{ path: 'user', redirectTo: '/user/1', pathMatch: 'full' },
+	{ path: 'user/', redirectTo: '/user/1', pathMatch: 'full' },
 	{ path: 'user/:id', component: UserComponent },
 	{ path: 'user/:id', component: UserComponent, children: USER_ROUTES },
-	{ path: '', component: HomeComponent }
+	{ path: '', component: HomeComponent },
+	{ path: '**', redirectTo: '/user/1111', pathMatch: 'full' }
+
 ]
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
