8d5303c6e517ffb4249b9d39b14ead6231b68077
bill r <jy@ozzis.us>
2017-02-20 12:35:56 -0500

    s9-l88

--- RoutingStart/src/app/app.component.ts
+++ RoutingStart/src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styles: [`
+        .active { color: red; border: solid 1px red }
+    `]
 })
 export class AppComponent {
 }
