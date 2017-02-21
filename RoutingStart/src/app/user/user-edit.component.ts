aa6789a7aa78ae5c1d2bf059eb895ad727893f7d
bill <jy@ozzis.us>
2017-02-21 17:38:35 -0500

    s9-l91

--- RoutingStart/src/app/user/user-edit.component.ts
+++ RoutingStart/src/app/user/user-edit.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {ComponentCanDeactivate} from "./user-edit.guard";
+import {Observable} from "rxjs/Rx";
 
 @Component({
     selector: 'app-user-edit',
     template: `
         <h3>User Edit</h3>
+        <button (click)="done = true">done: {{done}}</button>
+        <button (click)="goHome()">go home</button>
     `
 })
-export class UserEditComponent {
+export class UserEditComponent implements ComponentCanDeactivate {
+    done = false;
+    constructor(private router: Router) {}
+
+    goHome(){
+        this.router.navigate(['/']);
+    }
+
+    canDeactivate(): Observable<boolean> | boolean {
+
+        if(!this.done){
+            return confirm('done?');
+        }
+        return true;
+    }
 }
