804df21fc7bf753642b92541f952c091d9556a02
bill <jy@ozzis.us>
2017-02-22 16:44:33 -0500

    s10-l101

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { Recipe } from '../recipe';
-import { RecipeService } from '../';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe';
+import {RecipeService} from '../';
 
 @Component({
   selector: 'rb-recipe-list',
