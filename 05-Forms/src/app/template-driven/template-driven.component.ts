17ce49ccfc7f13e1a3c63396d9d655880355079c
bill <jy@ozzis.us>
2017-02-23 17:34:33 -0500

    s11-l108

--- 05-Forms/src/app/template-driven/template-driven.component.ts
+++ 05-Forms/src/app/template-driven/template-driven.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForm} from "@angular/forms";
 
 @Component({
-  selector: 'template-driven',
-  templateUrl: 'template-driven.component.html'
+	selector: 'template-driven',
+	templateUrl: 'template-driven.component.html',
+	styles: [`
+		input.ng-invalid { border: solid 1px #f88; }
+	`]
 })
 export class TemplateDrivenComponent {
-  onSubmit(form: NgForm){
-    console.log(form);
-  }
+	onSubmit(form:NgForm) {
+		console.log(form);
+	}
 }
