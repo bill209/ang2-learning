beb9941c24093fe5626a03e650a8e7aed2700778
bill <jy@ozzis.us>
2017-02-27 10:07:17 -0500

    s12-134

--- recipe-book/src/app/shopping-list/shopping-list-add.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "app/shared/ingredient";
+import {ShoppingListService} from "app/shopping-list/shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list-add',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 export class ShoppingListAddComponent implements OnInit {
 
   isAdd = true;
+  item: Ingredient;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient){
+  	console.log("this.isAdd",this.isAdd);
+  	
+    if(this.isAdd){
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    } else {
+
+    }
   }
 
 }
