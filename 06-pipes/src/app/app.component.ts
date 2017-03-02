4e103828db92ba85f465515c8f6d84aee2ae426e
bill <jy@ozzis.us>
2017-03-02 15:37:06 -0500

    s13-l146

---
+++ 06-pipes/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    .pipes { padding: 20px; margin: 20px; color: rebeccapurple }
+  `]
+})
+export class AppComponent {
+  title = 'app works!';
+  infamy = new Date(1941, 11, 7);
+}
