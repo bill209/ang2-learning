8d843dfdde22e2ef7125ba01bd76c2ca578dc174
bill r <jy@ozzis.us>
2017-03-04 16:11:16 -0500

    s13-l152 - fixed

--- 06-pipes/src/app/filter.pipe.ts
+++ 06-pipes/src/app/filter.pipe.ts
@@ -1,20 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    if(value.length === 0){
-      return value;
-    } else {
-      let resultArray = [];
-        if(value.match('^.*' + args + '.*$')) {
-          resultArray.push(value);
-        }
-      return resultArray;
-    }
-  }
+	transform(value: any, args?: any): any {
+		console.log("value",value);
+		console.log("args",args[0]);
+		
+		
+		if (value.length === 0) {
+			return value;
+		}
+		let resultArray = [];
+		for (let item of value) {
+			if (item.match('^.*' + args + '.*$')) {
+				resultArray.push(item);
+			}
+		}
+		return resultArray;
+	}
 
 }
