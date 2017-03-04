03cd30f617d5010235dd70b6c468487d290caa2a
bill r <jy@ozzis.us>
2017-03-04 16:37:23 -0500

    s13-l156

--- 06-pipes/src/app/app.component.ts
+++ 06-pipes/src/app/app.component.ts
@@ -13,4 +13,8 @@ export class AppComponent {
   infamy = new Date(1941, 11, 7);
   value = 3;
   values = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel'];
+
+  asyncValue = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('data is here'), 1000);
+  })
 }
--- 06-pipes/src/app/filter.pipe.ts
+++ 06-pipes/src/app/filter.pipe.ts
@@ -7,10 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
 	transform(value: any, args?: any): any {
-		console.log("value",value);
-		console.log("args",args[0]);
 
-		
 		if (value.length === 0) {
 			return value;
 		}
