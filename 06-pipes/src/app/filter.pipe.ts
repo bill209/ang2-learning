da7e1257b9e8a74bd4a283b8b677b7c689067756
bill r <jy@ozzis.us>
2017-03-04 16:15:51 -0500

    s13-l155

--- 06-pipes/src/app/filter.pipe.ts
+++ 06-pipes/src/app/filter.pipe.ts
@@ -2,14 +2,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'filter',
-  pure: false
+  pure: true
 })
 export class FilterPipe implements PipeTransform {
 
 	transform(value: any, args?: any): any {
 		console.log("value",value);
 		console.log("args",args[0]);
-		
+
 		
 		if (value.length === 0) {
 			return value;
